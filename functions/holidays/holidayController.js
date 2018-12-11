241147f8ff8bd6098c54ae6e30a3add260f4b6bc
shpiyu <piyushranjan95@gmail.com>
2018-12-11 14:39:00 +0530

    controller functions for holidays

---
+++ functions/holidays/holidayController.js
@@ -0,0 +1,68 @@
+const { holidays } = require('./holidaylist');
+
+exports.nextHoliday = function(date = new Date()) {
+    const keys = Object.keys(holidays);
+    for(let i=0; i<keys.length; i++) {
+        if (new Date(keys[i]) > date) {
+            return holidays[keys[i]];
+        }
+    }
+}
+
+exports.nextLongWeekend = function(date = new Date()) {
+    const keys = Object.keys(holidays);
+    for (let i=0 ;i<keys.length; i++) {
+        if (new Date(keys[i]) > date && canBeALongWeekend(new Date(keys[i]))) {
+            return getLongWeekends(new Date(keys[i]));
+        }
+    }
+}
+
+function canBeALongWeekend(date) {
+    return [1,2,4,5].indexOf(date.getDay());
+}
+
+function getLongWeekends(date) {
+    const holiday = holidays[date.toISOString()];
+    let leave = new Date(date);
+    let days;
+
+    switch(date.getDay()) {
+        case 2:
+            leave.setDate(date.getDate() - 1);
+            days = 4;
+            break;
+        case 4:
+            leave.setDate(date.getDate() + 1);
+            days = 4;
+            break; 
+        default:
+            leave = null;
+            days = 3;
+
+    }
+
+    return {
+        "holiday": holiday,
+        "leave": leave,
+        "days": days
+    }
+}
+
+exports.getHolidayList = function(date = new Date()) {
+    let list = Object.keys(holidays).map(holiday => new Date(holiday));
+    console.log(list);
+    let pos;
+    for(let i=0; i<list.length; i++) {
+        if (date < list[i]) {
+            pos = i;
+            break;
+        }
+    }
+    console.log(pos);
+
+    // TODO : returns a list of ISO date strings, should return hoiday object.
+    return list.slice(pos, ((pos + 5) > list.length ? list.length : pos + 5 ));
+    
+    
+}
--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,34 @@
-"use strict";
-const { dialogflow, Suggestions } = require("actions-on-google");
-const functions = require("firebase-functions");
-const app = dialogflow({ debug: true });
+'use strict';
+const {dialogflow, Suggestions} = require('actions-on-google');
+const functions = require('firebase-functions');
+const app = dialogflow({debug: true});
 const {
   searchMeetingRoom,
-  bookMeetingRoom
-} = require("./meeting_room/meetingRoomController");
+  bookMeetingRoom,
+} = require('./meeting_room/meetingRoomController');
 
 app.intent(
-  "search meeting room",
-  (conv, { person_count, date, duration, time }) => {
+  'search meeting room',
+  (conv, {person_count, date, duration, time}) => {
     // Present user with the corresponding basic card and end the conversation.
     const rooms = searchMeetingRoom(person_count, date, duration, time);
     conv.user.storage = {
       pc: person_count,
       date: date,
       duration: duration.amount,
-      time: time
+      time: time,
     };
     if (rooms.length > 0) {
-      conv.add("Please select your meeting room ");
-      const room_names = rooms.map(r => r.name);
+      conv.add('Please select your meeting room ');
+      const room_names = rooms.map((r) => r.name);
       conv.ask(new Suggestions(room_names));
     } else {
-      conv.close("Ghar ja");
+      conv.close('Ghar ja');
     }
   }
 );
 
-app.intent("search meeting room - book", (conv, { meeting_room }) => {
+app.intent('search meeting room - book', (conv, {meeting_room}) => {
   if (
     bookMeetingRoom(
       meeting_room,
@@ -37,11 +37,11 @@ app.intent("search meeting room - book", (conv, { meeting_room }) => {
     )
   ) {
     conv.ask(
-      "Great, your meeting room has been booked. bla bla, Would you like to do anything else?"
+     'Great, your meeting room has been booked. bla bla, Would you like to do anything else?'
     );
   } else {
     conv.ask(
-      "Sorry, the room could not be booked, would you like to do something else?"
+     'Sorry, the room could not be booked, would you like to do something else?'
     );
   }
 });
